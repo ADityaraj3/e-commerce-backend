0afd6f0d3b7b597d0d77b0dafae87d8612441718
rahul.p <rahul.p@tender247.com>
2025-09-01 19:05:17 +0530

    [MINOR] : basic setup

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+    app.enableCors({
+        origin: '*',
+    });
+    const config = new DocumentBuilder()
+        .setTitle('E-Com')
+        .setDescription('API documentation for E-Com backend')
+        .setVersion('1.0')
+        .addBearerAuth()
+        .build();
+
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document);
+
+    await app.listen(8989);
+    console.log(`🎯 http://localhost:${8989}/api`);
 }
 bootstrap();
