8d70b39910757fe1e38127a70a3929b8e414870e
rahul.p <rahul.p@tender247.com>
2025-09-03 17:08:13 +0530

    [ CHANGE ] : for exception

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './modules/prisma/prisma.module';
+import { APP_FILTER } from '@nestjs/core';
+import { AllExceptionFilter } from './utils/exceptionFilter.service';
+import { CustomLogger } from './utils/logger.service';
+import { CUSTOM_LOGGER } from './utils/constant';
 
 @Module({
   imports: [PrismaModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    { provide: APP_FILTER, useClass: AllExceptionFilter },
+    { provide: CUSTOM_LOGGER, useClass: CustomLogger },
+  ],
 })
 export class AppModule {}
